76169000c78a882a8d6a635e8777f20325cf1ad6
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 11:58:42 +0800

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 function App() {
   // Hook of useState to hook the value
   // []=[] - destructuring
+  // setCount = function to update the value of count
   const [count, setCount] = useState(0);
   //console.log(count);
 
   function increase() {
+    // update the value of count
     setCount(count + 1);
   }
 
@@ -17,6 +19,7 @@ function App() {
   return (
     <div className="container">
       <h1>{count}</h1>
+      {/* button that call the function when clicked */}
       <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
