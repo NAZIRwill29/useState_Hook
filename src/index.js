65e1b53e843e07ff8b4d76f4632e389e97e9e995
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 11:28:04 +0800

    add btn with function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var count = 0;
+
+function increase() {
+  console.log("increased");
+}
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
