94de81cd407a637065e2db0a218948eb87f331a7
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 11:55:16 +0800

    change the value more proefficiently increase and decrease

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,21 @@ import React, { useState } from "react";
 function App() {
   // Hook of useState to hook the value
   // []=[] - destructuring
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   //console.log(count);
 
   function increase() {
-    setCount(12);
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
   }
 
   return (
     <div className="container">
       <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
   );
